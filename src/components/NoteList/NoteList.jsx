5fbd826fba8d001567c1e7e416430e5265d9e444
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-02-08 12:38:55 -0800

    added post it background to note cards

--- src/components/NoteList/NoteList.jsx
+++ src/components/NoteList/NoteList.jsx
@@ -4,14 +4,16 @@ import styles from './NoteList.css';
 
 export default function NoteList() {
   const { notes } = useNote();
-  const { note_card } = styles;
+  const { note_card, note_text } = styles;
 
   return (
     <div>
       {notes?.map((note) => (
         <div key={`${note.name}-${note.message}`} className={note_card}>
-          <p>-{note.name}</p>
-          <p>{note.message}</p>
+          <div className={note_text}>
+            <p>-{note.name}</p>
+            <p>{note.message}</p>
+          </div>
         </div>
       ))}
     </div>
