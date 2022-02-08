92b718db1cb75d7b6fb619b2ef8718a16bcca96f
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-02-08 15:09:29 -0800

    added background styling and centered note list

--- src/components/NoteList/NoteList.jsx
+++ src/components/NoteList/NoteList.jsx
@@ -4,10 +4,10 @@ import styles from './NoteList.css';
 
 export default function NoteList() {
   const { notes } = useNote();
-  const { note_card, note_text } = styles;
+  const { note_card, note_text, note_list } = styles;
 
   return (
-    <div>
+    <div className={note_list}>
       {notes?.map((note) => (
         <div key={`${note.name}-${note.message}`} className={note_card}>
           <div className={note_text}>
