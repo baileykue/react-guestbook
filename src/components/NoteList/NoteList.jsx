b7190d2cd2484d1bc99b840d6c439b08ec0dc739
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-02-21 17:59:06 -0800

    redirect working yay

--- src/components/NoteList/NoteList.jsx
+++ src/components/NoteList/NoteList.jsx
@@ -2,17 +2,21 @@ import { useNote } from '../../context/NoteContext';
 
 import React from 'react';
 import styles from './NoteList.css';
+import { useUser } from '../../context/UserContext';
 
 export default function NoteList() {
   const { notes } = useNote();
+  const {
+    user: { email },
+  } = useUser();
   const { note_card, note_text, note_list } = styles;
 
   return (
     <div className={note_list}>
       {notes?.map((note) => (
-        <div key={`${note.name}-${note.message}`} className={note_card}>
+        <div key={`${email}-${note.message}`} className={note_card}>
           <div className={note_text}>
-            <p>-{note.name}</p>
+            <p>-{email}</p>
             <p>
               {note.message} {note.emoji}
             </p>
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
 
 export default function PrivateRoute({ children, ...restProps }) {
